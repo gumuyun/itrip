96e7580ba95c232266878b676ee33b3bd4d4949e
gumuyun <gumuyun@qq.com>
2019-02-19 17:58:18 +0800

    更新User类.

--- src/main/java/cn/bdqn/itrip/pojo/User.java
+++ src/main/java/cn/bdqn/itrip/pojo/User.java
@@ -2,5 +2,30 @@ package cn.bdqn.itrip.pojo;
 
 import java.io.Serializable;
 
+/**
+ *
+ * 用户类
+ * @author gumuyun
+ */
 public class User implements Serializable {
+
+    private Integer id;
+    private String userName;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
